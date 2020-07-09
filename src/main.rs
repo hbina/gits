7b264422ca61e2a01c47a895dcc3067b8c156826
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-07-09 07:28:44 -0400

    Fixed spelling and output now have colors!

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
     let result = std::process::Command::new("git")
-        .args(&["status"])
+        .args(&["-c","color.status=always","status"])
         .output()
         .expect("failed to execute process")
         .stdout;
-        
+
     println!(
         "{}",
         String::from_utf8(result).expect("unable to parse string.")
